458498722e36e4dd9d324ed401558d28accaec58
Wilson Luiz Prosdocimo <wilson@wprosdocimo.com.br>
2021-01-26 18:18:52 -0300

    Ajustes e alteração do logo

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import Link from "next/link";
 import db from '../db.json';
 // import Widget from '../src/components/Widget'
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Widget from '../src/components/Widget';
 
 export const QuizContainer = styled.div`
   width: 100%;
-  max-width: 80vw;
+  max-width: 350px;
   padding-top: 45px;
   margin: auto 10%;
   @media screen and (max-width: 500px) {
-    max-width: 350px;
     margin: auto;
     padding: 15px;
   }
 `;
 
 export default function Quiz() {
+  const router = useRouter();
+  const { name } = router.query;
+
   return (
     <QuizBackground backgroundImage={db.bg}>
       <Head>
@@ -32,8 +37,17 @@ export default function Quiz() {
         <meta property="og:title" content="Quiz sobre Pneus" />
       </Head>
       <QuizContainer>
-        <QuizLogo />
-
+        <Link href="/">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a>
+            <QuizLogo />
+          </a>
+        </Link>
+        <Widget>
+          <Widget.Header>
+            <h1>{name}</h1>
+          </Widget.Header>
+        </Widget>
         <Footer />
       </QuizContainer>
       <GitHubCorner projectUrl="https://github.com/wprosdocimo" />
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -32,6 +32,9 @@ Widget.Header = styled.header`
   }
 `;
 
+Widget.Image = styled.image`
+`;
+
 Widget.Content = styled.div`
   padding: 24px 32px 32px 32px;
   & > *:first-child {
